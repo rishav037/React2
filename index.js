615b48b4d85782cca62b235d4fc2aa9c771ca69e
Rishav Kumar <98388799+rishav037@users.noreply.github.com>
2022-03-24 22:44:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from "react"; //new way to import not to use require
+import ReactDOM from "react-dom";
+
+ReactDOM.render(<h1>HELLO WORLD</h1>, document.getElementById("root"));
+
+// var h1 = document.createElement("h1"); //PURE JS CODE
+// h1.innerHTML = "HELLO WORLD";
+// document.getElementById("root").appendChild(h1);
